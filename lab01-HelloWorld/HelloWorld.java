642e06c87eb734d5c5f281661ef4195f4c7b5e5c
alc218 <alc218@lehigh.edu>
2015-01-23 15:54:35 +0000

    HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////
+//Allen Chan
+//Lab 1
+//Hello World Java Program
+//
+//
+//
+public class HelloWorld{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("###############################");
+        System.out.println("###############################");
+        System.out.println("#                             #");
+        System.out.println("#      Hello World 2015!!     #");
+        System.out.println("#                             #");
+        System.out.println("###############################");
+        System.out.println("###############################");
+        System.out.println("test");
+        
+}
+}
