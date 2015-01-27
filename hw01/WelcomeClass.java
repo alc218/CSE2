a141b05fb9e728a34e554562a71d34cfc3051517
alc218 <alc218@lehigh.edu>
2015-01-27 02:21:52 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////
+//Allen Chan
+//Homework 1
+//Welcome Class Java Program
+//
+//
+//
+public class WelcomeClass{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("-------------------");
+        System.out.println("    | WELCOME |   ");
+        System.out.println("-------------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-A--L--C--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Just finished doing my first computer science homework");
+        System.out.println("and I am excited to write more in the future!");
+    }
+}
